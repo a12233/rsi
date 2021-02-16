083cd293df84ecaf1d45653b531f797e2f59ca24
a12233 <lirex14@gmail.com>
2021-02-16 00:55:52 +0000

    sceener

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-from finsymbols import symbols
+# from finsymbols import symbols
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators 
 from pandas import DataFrame, Series
@@ -9,8 +9,8 @@ import yfinance as yf
 # import talib 
 import pandas as pd
 import numpy as np
-from functools import lru_cache
-from polygon import RESTClient
+# from functools import lru_cache
+# from polygon import RESTClient
 import pandas_datareader as pdr
 import json
 from datetime import datetime, timedelta
@@ -36,15 +36,16 @@ sp500List = [['MMM'], ['ABT'], ['ABBV'], ['ABMD'], ['ACN'], ['ATVI'], ['ADBE'], 
 
 def test(): 
     sp500List = [['MMM'], ['ABT'], ['ABBV'], ['ABMD'], ['ACN'], ['ATVI'], ['ADBE'], ['AMD'], ['AAP'], ['AES'], ['AFL'], ['A'], ['APD'], ['AKAM'], ['ALK'], ['ALB'], ['ARE'], ['ALXN'], ['ALGN'], ['ALLE'], ['LNT'], ['ALL'], ['GOOGL'], ['GOOG'], ['MO'], ['AMZN'], ['AMCR'], ['AEE'], ['AAL'], ['AEP'], ['AXP'], ['AIG'], ['AMT'], ['AWK'], ['AMP'], ['ABC'], ['AME'], ['AMGN'], ['APH'], ['ADI'], ['ANSS'], ['ANTM'], ['AON'], ['AOS'], ['APA'], ['AIV'], ['AAPL'], ['AMAT'], ['APTV'], ['ADM'], ['ANET'], ['AJG'], ['AIZ'], ['T'], ['ATO'], ['ADSK'], ['ADP'], ['AZO'], ['AVB'], ['AVY'], ['BKR'], ['BLL'], ['BAC'], ['BK'], ['BAX'], ['BDX'], ['BRK-B'], ['BBY'], ['BIO'], ['BIIB'], ['BLK'], ['BA'], ['BKNG'], ['BWA'], ['BXP'], ['BSX'], ['BMY'], ['AVGO'], ['BR'], ['BF-B'], ['CHRW'], ['COG'], ['CDNS'], ['CPB'], ['COF'], ['CAH'], ['KMX'], ['CCL'], ['CARR'], ['CTLT'], ['CAT'], ['CBOE'], ['CBRE'], ['CDW'], ['CE'], ['CNC'], ['CNP'], ['CERN'], ['CF'], ['SCHW'], ['CHTR'], ['CVX'], ['CMG'], ['CB'], ['CHD'], ['CI'], ['CINF'], ['CTAS'], ['CSCO'], ['C'], ['CFG'], ['CTXS'], ['CLX'], ['CME'], ['CMS'], ['KO'], ['CTSH'], ['CL'], ['CMCSA'], ['CMA'], ['CAG'], ['CXO'], ['COP'], ['ED'], ['STZ'], ['COO'], ['CPRT'], ['GLW'], ['CTVA'], ['COST'], ['CCI'], ['CSX'], ['CMI'], ['CVS'], ['DHI'], ['DHR'], ['DRI'], ['DVA'], ['DE'], ['DAL'], ['XRAY'], ['DVN'], ['DXCM'], ['FANG'], ['DLR'], ['DFS'], ['DISCA'], ['DISCK'], ['DISH'], ['DG'], ['DLTR'], ['D'], ['DPZ'], ['DOV'], ['DOW'], ['DTE'], ['DUK'], ['DRE'], ['DD'], ['DXC'], ['EMN'], ['ETN'], ['EBAY'], ['ECL'], ['EIX'], ['EW'], ['EA'], ['EMR'], ['ETR'], ['EOG'], ['EFX'], ['EQIX'], ['EQR'], ['ESS'], ['EL'], ['ETSY'], ['EVRG'], ['ES'], ['RE'], ['EXC'], ['EXPE'], ['EXPD'], ['EXR'], ['XOM'], ['FFIV'], ['FB'], ['FAST'], ['FRT'], ['FDX'], ['FIS'], ['FITB'], ['FE'], ['FRC'], ['FISV'], ['FLT'], ['FLIR'], ['FLS'], ['FMC'], ['F'], ['FTNT'], ['FTV'], ['FBHS'], ['FOXA'], ['FOX'], ['BEN'], ['FCX'], ['GPS'], ['GRMN'], ['IT'], ['GD'], ['GE'], ['GIS'], ['GM'], ['GPC'], ['GILD'], ['GL'], ['GPN'], ['GS'], ['GWW'], ['HAL'], ['HBI'], ['HIG'], ['HAS'], ['HCA'], ['PEAK'], ['HSIC'], ['HSY'], ['HES'], ['HPE'], ['HLT'], ['HFC'], ['HOLX'], ['HD'], ['HON'], ['HRL'], ['HST'], ['HWM'], ['HPQ'], ['HUM'], ['HBAN'], ['HII'], ['IEX'], ['IDXX'], ['INFO'], ['ITW'], ['ILMN'], ['INCY'], ['IR'], ['INTC'], ['ICE'], ['IBM'], ['IP'], ['IPG'], ['IFF'], ['INTU'], ['ISRG'], ['IVZ'], ['IPGP'], ['IQV'], ['IRM'], ['JKHY'], ['J'], ['JBHT'], ['SJM'], ['JNJ'], ['JCI'], ['JPM'], ['JNPR'], ['KSU'], ['K'], ['KEY'], ['KEYS'], ['KMB'], ['KIM'], ['KMI'], ['KLAC'], ['KHC'], ['KR'], ['LB'], ['LHX'], ['LH'], ['LRCX'], ['LW'], ['LVS'], ['LEG'], ['LDOS'], ['LEN'], ['LLY'], ['LNC'], ['LIN'], ['LYV'], ['LKQ'], ['LMT'], ['L'], ['LOW'], ['LUMN'], ['LYB'], ['MTB'], ['MRO'], ['MPC'], ['MKTX'], ['MAR'], ['MMC'], ['MLM'], ['MAS'], ['MA'], ['MKC'], ['MXIM'], ['MCD'], ['MCK'], ['MDT'], ['MRK'], ['MET'], ['MTD'], ['MGM'], ['MCHP'], ['MU'], ['MSFT'], ['MAA'], ['MHK'], ['TAP'], ['MDLZ'], ['MNST'], ['MCO'], ['MS'], ['MOS'], ['MSI'], ['MSCI'], ['MYL'], ['NDAQ'], ['NOV'], ['NTAP'], ['NFLX'], ['NWL'], ['NEM'], ['NWSA'], ['NWS'], ['NEE'], ['NLSN'], ['NKE'], ['NI'], ['NSC'], ['NTRS'], ['NOC'], ['NLOK'], ['NCLH'], ['NRG'], ['NUE'], ['NVDA'], ['NVR'], ['ORLY'], ['OXY'], ['ODFL'], ['OMC'], ['OKE'], ['ORCL'], ['OTIS'], ['PCAR'], ['PKG'], ['PH'], ['PAYX'], ['PAYC'], ['PYPL'], ['PNR'], ['PBCT'], ['PEP'], ['PKI'], ['PRGO'], ['PFE'], ['PM'], ['PSX'], ['PNW'], ['PXD'], ['PNC'], ['POOL'], ['PPG'], ['PPL'], ['PFG'], ['PG'], ['PGR'], ['PLD'], ['PRU'], ['PEG'], ['PSA'], ['PHM'], ['PVH'], ['QRVO'], ['PWR'], ['QCOM'], ['DGX'], ['RL'], ['RJF'], ['RTX'], ['O'], ['REG'], ['REGN'], ['RF'], ['RSG'], ['RMD'], ['RHI'], ['ROK'], ['ROL'], ['ROP'], ['ROST'], ['RCL'], ['SPGI'], ['CRM'], ['SBAC'], ['SLB'], ['STX'], ['SEE'], ['SRE'], ['NOW'], ['SHW'], ['SPG'], ['SWKS'], ['SLG'], ['SNA'], ['SO'], ['LUV'], ['SWK'], ['SBUX'], ['STT'], ['STE'], ['SYK'], ['SIVB'], ['SYF'], ['SNPS'], ['SYY'], ['TMUS'], ['TROW'], ['TTWO'], ['TPR'], ['TGT'], ['TEL'], ['FTI'], ['TDY'], ['TFX'], ['TER'], ['TXN'], ['TXT'], ['TMO'], ['TIF'], ['TJX'], ['TSCO'], ['TT'], ['TDG'], ['TRV'], ['TFC'], ['TWTR'], ['TYL'], ['TSN'], ['UDR'], ['ULTA'], ['USB'], ['UAA'], ['UA'], ['UNP'], ['UAL'], ['UNH'], ['UPS'], ['URI'], ['UHS'], ['UNM'], ['VFC'], ['VLO'], ['VAR'], ['VTR'], ['VRSN'], ['VRSK'], ['VZ'], ['VRTX'], ['VIAC'], ['V'], ['VNT'], ['VNO'], ['VMC'], ['WRB'], ['WAB'], ['WMT'], ['WBA'], ['DIS'], ['WM'], ['WAT'], ['WEC'], ['WFC'], ['WELL'], ['WST'], ['WDC'], ['WU'], ['WRK'], ['WY'], ['WHR'], ['WMB'], ['WLTW'], ['WYNN'], ['XEL'], ['XRX'], ['XLNX'], ['XYL'], ['YUM'], ['ZBRA'], ['ZBH'], ['ZION'], ['ZTS']]
-    ti = TechIndicators(key=API_KEY, output_format='pandas')
-    ts = TimeSeries(key=API_KEY, output_format='pandas')
-    data, meta_data = ts.get_daily(symbol='AMZN',outputsize='compact')
-    lastPrice = data.head(1)['4. close']
-    pprint(lastPrice)
+    for i in sp500List:
+        ti = TechIndicators(key=API_KEY, output_format='pandas')
+        ts = TimeSeries(key=API_KEY, output_format='pandas')
+        data, _ = ts.get_daily(symbol='AMZN',outputsize='compact')
+        lastPrice = data.head(1)['4. close']
+        sma20, meta_data = ti.get_sma(symbol='AMZN', interval='daily', time_period=20,series_type='close')
+        sma60, _ = ti.get_sma(symbol='AMZN', interval='daily', time_period=60,series_type='close')
+        sma120, _ = ti.get_sma(symbol='AMZN', interval='daily', time_period=120,series_type='close')
 
-    data, meta_data = ti.get_wma(symbol='AMZN', interval='daily', time_period=200,series_type='close')
-    lastWma = data.tail(1).WMA
-    pprint(lastPrice/lastWma)
+
 
 # def testTALib(): 
 #     close = numpy.random.random(100)
@@ -61,6 +62,18 @@ def writeCSV():
         
         write.writerow(fields) 
         write.writerows(sp500List) 
+
+def yFinanceSMA():
+    symbol = 'AAPL'
+    data = yf.Ticker(symbol).history(period="max")
+    lastPrice = data['Close'].tail(1).item()
+    sma20 = data.tail(20)['Close'].mean()
+    sma60 = data.tail(60)['Close'].mean()
+    sma120 = data.tail(20)['Close'].mean()
+
+    res = {"C/S" : lastPrice/sma20, "S/M":sma20/sma60, "M/L":sma60/sma120}
+    pprint(res)
+
 
 def testYfinance(): 
     symbol = 'AAPL'
@@ -277,4 +290,6 @@ if __name__ == "__main__":
     # testDiff()
     # a = pd.Timestamp.now() - offsets.YearBegin()
     # print(a)
-    getPriceChange()
+    # getPriceChange()
+    # test() 
+    yFinanceSMA()
